292407bf440f37b3282bebbc8b429eb36308fa4c
ppate304 <patel.pari.97@gmail.com>
2021-11-16 15:26:33 -0600

    first commit

--- js/api.js
+++ js/api.js
@@ -1,25 +1,26 @@
 
 
-// async function doAPIcall(City){
-//     result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=5afdd11ae9b46fbddbedb86a2dff39e0`)
-//         .catch((e)=>{console.error(e);alert("")})
-//             .finally(console.log("API request is Over"))
-//     console.log(result)
+async function doAPICall(){
+    let City=document.querySelector('#City').value
+    result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${City}&units=imperial&appid=ac6d4d801330b08597ceb683818b8fbe`)
+       .catch((e)=>{console.error(e);alert("Bad city name!")})
+           .finally(console.log("API request is Over"))
+       console.log(result)
+       console.log(result.data)
 
-async function doAPICall(City){
-    // let city=document.querySelector('#inputvalue').value
-    result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${City}&units=imperial&appid=144b7f905c903cea963eded9cd974aa9`)
-        .catch((e)=>{console.error(e);alert("Bad city name!!!!!")})
-            .finally(console.log("API request is Over"))
-        console.log(result)
+   
+   temperature=document.getElementById('temperature')
+   temperature.innerText = result.data.main.temp
 
-    // result= result.data
-    // console.log(result.data)
-temperature=result.main.temp
-document.getElementById('temperature').temperature.innterText = result.main.temp
+   description = document.getElementById('description');
+   description.innerText = result.data.weather[0].description
 
-description = document.getElementById('description');
-description.innerText = result.weather.main
+   temp_min = document.getElementById('temp_min');
+   temp_min.innerText = result.data.main.temp_min
+
+   temp_max = document.getElementById('temp_max');
+   temp_max.innerText = result.data.main.temp_max
+
 
 }
 // function handleSubmit(){
